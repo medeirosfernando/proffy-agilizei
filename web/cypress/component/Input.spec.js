0e9548353aa35bfec40864e4466970baeeb9f843
Fernando Medeiros <fernando.medeiros@mzgroup.com>
2020-12-02 11:58:33 -0200

    impl: teste de componente para o Input

--- web/cypress/component/Input.spec.js
+++ web/cypress/component/Input.spec.js
@@ -1,38 +1,35 @@
 /// <reference types="cypress" />
 
 import React from 'react'
-import PageHeader from '../../src/components/PageHeader'
+import PageHeader from '../../src/components/PageHeader';
 import Input from '../../src/components/Input'
 import { mount } from 'cypress-react-unit-test'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-
 context('Input component', () => {
     const baseCss = '/__root/src/assets/styles/global.css'
-    const indexCss = '/__root/src/components/Input/style.css'
+    const indexCss = '/__root/src/components/Input/styles.css'
 
     it('deve ser renderizado com sucesso', () => {
         const label = 'Nome Completo'
-
-        const title = "Que incrível que você quer dar aulas."
-        const description = "O primeiro passo é preencher esse formulário de inscrição."
+        const input = 'name'
 
         mount(
             <Router>
-                <Input>
-                    <label htmlFor={name}>{label}</label>
-                    <input type="text" id={name}/>
-                    </Input>
+                <PageHeader></PageHeader>                            
+                    <Input
+                        label= { label }
+                        input= { name }
+                    />                
             </Router>
             ,
             {
                 stylesheets: [baseCss, indexCss]
             }
         )
-
-        cy.get('form .input-block label').as('label')
+        cy.get('.input-block label').as('label')
         cy.get('@label').then(($elemento) => {
-            expect($elemento.css('color')).to.eq('rgb(156, 152, 166)')
+            expect($elemento.css('font')).to.eq('500 14px Poppins')
         })
-    });
-});
+    })
+})
